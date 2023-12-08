147fa573874be6cda1b12c5621c061e2a9fd736c
Lovingcats <whehdrms6810@naver.com>
2023-12-08 13:22:16 +0900

    [CREATE] dynamic_typing_example.dart

---
+++ Dart/typing_example/dynamic_typing_example.dart
@@ -0,0 +1,18 @@
+void main() {
+  // 동적 타이핑을 사용하여 변수 선언
+  dynamic dynamicVariable = 42;
+
+  // 동적 타이핑에 따라 여러가지 값을 할당
+  dynamicVariable = "Hello";
+  print(dynamicVariable); //Hello
+  dynamicVariable = true;
+  print(dynamicVariable); //true
+  dynamicVariable = 12.9;
+  print(dynamicVariable); //12.9
+  dynamicVariable = null;
+  print(dynamicVariable); //null
+  dynamicVariable = "Success!";
+  print(dynamicVariable); //Success!
+
+  //동적 타이핑을 사용하면 여러가지 타입 값을 계속 재할당 할 수 있음
+}
